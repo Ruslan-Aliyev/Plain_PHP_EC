9026004463ca3e833f8bbbd7ed960f7d80b220a9
Ruslan <ruslan@Ruslans-MacBook-Pro.local>
2021-05-26 18:10:59 +0700

    update

--- index.php
+++ index.php
@@ -6,11 +6,16 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Omnipay\Omnipay;
 
-if ($_POST)
-{
-    $stripeGateway = Omnipay::create('Stripe');
-    $stripeGateway->setApiKey('sk_test_51ISw50HC8M2JxTUFZ0fNbVPvrEMM8ld25Ntemq53sSxyKSOggo2RYs71mpgYASRaxevWSwieCmZPDI8Hs6UOxLWV003IcdkMas');
+$stripeGateway = Omnipay::create('Stripe');
+$stripeGateway->setApiKey('sk_test_51ISw50HC8M2JxTUFZ0fNbVPvrEMM8ld25Ntemq53sSxyKSOggo2RYs71mpgYASRaxevWSwieCmZPDI8Hs6UOxLWV003IcdkMas');
 
+$paypalGateway = Omnipay::create('PayPal_Rest');
+$paypalGateway->setClientId('AYzhvnHiHYcf215rSaLsUktSWO9AZ_848z9crMoVO5bk6Tr_9xiFjPcTkdSNej3C67EJ23O-CDBnsRU4');
+$paypalGateway->setSecret('EMnGiSlyXijukNaswbgcPcJ7o_0LweOb9PBjK6MRojX1frlL5RDMHxsRKl-v5nunuP50GpVNMp07G-gl');
+$paypalGateway->setTestMode(true);
+
+if (isset($_POST['gateway']) && $_POST['gateway'] === 'Stripe')
+{
     $token = $_POST['stripeToken']; 
 
     $response = $stripeGateway->purchase([
@@ -22,7 +27,57 @@ if ($_POST)
     dd($response);
 }
 
+if (isset($_POST['gateway']) && $_POST['gateway'] === 'Paypal')
+{
+    $response = $paypalGateway->purchase(array(
+        'amount' => $_POST['amount'],
+        'currency' => 'USD',
+        'returnUrl' => getBaseUrl() . 'index.php',
+        'cancelUrl' => getBaseUrl() . 'index.php',
+    ))->send();
+
+    if ($response->isRedirect()) 
+    {
+        $response->redirect();
+    } 
+    else 
+    {
+        dd( $response->getMessage() );
+    }
+}
+
+if (array_key_exists('paymentId', $_GET) && array_key_exists('PayerID', $_GET)) 
+{
+    $transaction = $paypalGateway->completePurchase(array(
+        'payer_id'             => $_GET['PayerID'],
+        'transactionReference' => $_GET['paymentId'],
+    ));
+    $response = $transaction->send();
+ 
+    if ($response->isSuccessful()) 
+    { // The customer has successfully paid.
+        dd( $response->getData() );
+
+        dd("Payment is successful. Your transaction id is: " . $payment_id);
+    } 
+    else 
+    {
+        dd( $response->getMessage() );
+    }
+}
+
 // ---
+
+function getBaseUrl() 
+{
+    $baseUrl = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    $urlParts = explode('/', $baseUrl);
+    array_pop($urlParts);
+    $baseUrl = implode('/', $urlParts);
+    $baseUrl .= '/';
+
+    return $baseUrl;
+}
 
 function dd($content) 
 {
@@ -39,45 +94,65 @@ function dd($content)
         <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
         <title>Stripe Payment</title>
         <script src="https://js.stripe.com/v3/"></script>
+        <style>
+            .box {
+                margin: 20px;
+                border: 1px solid black;
+                padding: 20px;
+            }
+        </style>
     </head>
     <body>
-        <div id="paymentResponse"></div>
-        
-        <form action="" method="POST" id="paymentFrm">
-            <div class="form-group">
-                <label>AMOUNT</label>
-                <input type="number" name="amount" id="amount" step="0.01" class="field" placeholder="Enter amount" required="" autofocus="">
-            </div>
-            <div class="form-group">
-                <label>EMAIL</label>
-                <input type="email" name="email" id="email" class="field" placeholder="Enter email" required="">
-            </div>
+        <div class="box">
+            <h3>Pay by Stripe</h3>
 
-            <!-- This -->
-            <div class="form-group">
-                <label>CARD NUMBER</label>
-                <div id="card_number" class="field"></div>
-            </div>
-            <div class="row">
-                <div class="left">
-                    <div class="form-group">
-                        <label>EXPIRY DATE</label>
-                        <div id="card_expiry" class="field"></div>
+            <div id="paymentResponse"></div>
+            
+            <form action="" method="POST" id="stripeForm">
+                <div class="form-group">
+                    <label>AMOUNT</label>
+                    <input type="number" name="amount" id="amount" step="0.01" class="field" placeholder="Enter amount" required="" autofocus="">
+                </div>
+
+                <div class="form-group">
+                    <label>CARD NUMBER</label>
+                    <div id="card_number" class="field"></div>
+                </div>
+                <div class="row">
+                    <div class="left">
+                        <div class="form-group">
+                            <label>EXPIRY DATE</label>
+                            <div id="card_expiry" class="field"></div>
+                        </div>
+                    </div>
+                    <div class="right">
+                        <div class="form-group">
+                            <label>CVC CODE</label>
+                            <div id="card_cvc" class="field"></div>
+                        </div>
                     </div>
                 </div>
-                <div class="right">
-                    <div class="form-group">
-                        <label>CVC CODE</label>
-                        <div id="card_cvc" class="field"></div>
-                    </div>
-                </div>
-            </div>
-            <!-- Or: Whole generic card input element, instead of 1 by 1 -->
-            <!-- <div id="card-element"></div> -->
-            <!--  -->
 
-            <button type="submit" class="btn btn-success" id="payBtn">Submit Payment</button>
-        </form>
+                <input type="hidden" name="gateway" value="Stripe">
+
+                <button type="submit" class="btn btn-success" id="payBtn">Pay by Stripe</button>
+            </form>
+        </div>
+
+        <div class="box">
+            <h3>Pay by Paypal</h3>
+
+            <form action="" method="POST" id="paypalForm">
+                <div class="form-group">
+                    <label>AMOUNT</label>
+                    <input type="number" name="amount" id="amount" step="0.01" class="field" placeholder="Enter amount" required="" autofocus="">
+                </div>
+
+                <input type="hidden" name="gateway" value="Paypal">
+
+                <button type="submit" class="btn btn-success" id="payBtn">Pay by Paypal</button>
+            </form>
+        </div>
 
         <script type="text/javascript">
             var stripe = Stripe('pk_test_51ISw50HC8M2JxTUFdmtaIrYoz714yudM5V9s2U3SEqTN0ugI3TnlVP7Oxj3KAMLNLsYe6GAvC2FpCJ30OheFkLqG00tuhMBMUr'); // Public Key
@@ -101,7 +176,6 @@ function dd($content)
 
             var elements = stripe.elements();
 
-            // ---- This ----
             var cardElement = elements.create('cardNumber', {
                 style: style
             });
@@ -113,10 +187,6 @@ function dd($content)
             elements.create('cardCvc', {
                 'style': style
             }).mount('#card_cvc');
-            // ---- Or ----
-            // Whole generic card input element, instead of 1 by 1
-            // elements.create('card', {style: style}).mount('#card-element');
-            // ----  ----
 
             // Validate input of the card elements
             var resultContainer = document.getElementById('paymentResponse');
@@ -128,8 +198,8 @@ function dd($content)
                 }
             });
 
-            var form = document.getElementById('paymentFrm');
-            form.addEventListener('submit', function(e) {
+            var stripeForm = document.getElementById('stripeForm');
+            stripeForm.addEventListener('submit', function(e) {
                 e.preventDefault();
 
                 stripe.createToken(cardElement).then(function(result) {
@@ -150,8 +220,8 @@ function dd($content)
                 hiddenInput.setAttribute('type', 'hidden');
                 hiddenInput.setAttribute('name', 'stripeToken');
                 hiddenInput.setAttribute('value', token.id);
-                form.appendChild(hiddenInput);
-                form.submit();
+                stripeForm.appendChild(hiddenInput);
+                stripeForm.submit();
             }
         </script>
 
--- readme.md
+++ readme.md
@@ -1,9 +1,50 @@
 # E-Commerce & Plain PHP
 
+## Omnipay
+
 `composer require league/omnipay omnipay/paypal omnipay/stripe`
 
-- https://www.youtube.com/watch?v=_7YBIRQfSN0
-- https://www.youtube.com/watch?v=5AbkSomC-a4
+See `index.php` here.
+
+https://developer.paypal.com/developer/applications/  
+(after https://developer.paypal.com/developer/accounts/)
+
+https://dashboard.stripe.com/
+
+## Tutorials
+
+- Stripe
+	- Plain PHP
+		- https://github.com/Ruslan-Aliyev/Stripe-API-Plain-PHP
+	- Laravel
+		- https://medium0.com/@juangsalazprabowo/how-to-integrate-laravel-with-stripe-fc54e54a767c
+	- Misc
+		- https://stackoverflow.com/questions/29851706/how-to-create-a-customer-in-omnipay-stripe
+- Paypal
+	- Plain PHP
+		- https://www.youtube.com/watch?v=5AbkSomC-a4
+	- Laravel
+		- https://www.youtube.com/watch?v=_7YBIRQfSN0
+		- https://sujipthapa.co/blog/a-guide-to-integrate-omnipay-paypal-with-laravel
+	- WP
+		- https://github.com/atabegruslan/WP_WooCommerce
+	- Magento 2
+		- https://github.com/atabegruslan/Magento
+
+## Database design
 
 - https://moqups.com/templates/diagrams-flowcharts/erd/ecommerce-database-diagram/
 - https://creately.com/diagram/example/he7cxejx1/e-Commerce%20Database
+
+## Plugins
+
+- https://github.com/srmklive/laravel-paypal
+- https://laravel-vuejs.com/paystack-payment-gateway-plugin/
+- https://www.positronx.io/how-to-integrate-paypal-payment-gateway-in-laravel/
+- https://github.com/shetabit/payment
+- https://codecanyon.net/search/php%20stripe%20payments
+- https://www.expresstechsoftwares.com/integrate-laravel-stripe-payment-gateway-online-payments/
+- https://laravel.com/docs/8.x/billing
+- https://onlinewebtutorblog.com/stripe-payment-gateway-integration-in-laravel-8/
+- https://www.tutsmake.com/laravel-8-stripe-payment-gateway-integration-tutorial/
+- https://github.com/rap2hpoutre/laravel-stripe-connect
